155a9a7d3558306f9581a616f886f7fec0f34ee5
pangtinghuan <pangtinghuan@tuandai.com>
2019-03-01 11:42:34 +0800

    增加了集合的isEmpty()测试，以及iterator()测试

--- src/test/java/com/drafire/collection/TestCollection.java
+++ src/test/java/com/drafire/collection/TestCollection.java
@@ -8,25 +8,42 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestCollection {
     //ArrayList 是由数组组成
-    private Collection<Person> arrayList = new ArrayList<>();
+    private Collection<Person> arrayList = new LinkedList<>();
     //链表
     private Collection<Person> linkedList = new LinkedList<>();
 
     private static final int NUM = 1000000;
 
+
     @Test
     public void testAdd() {
         /**
-         * 可以看出，如果是添加数据到集合里面，LinkedList比ArrayList更快；
+         * 可以看出，如果是添加数据到集合里面，LinkedList比ArrayList 要慢，因为都是添加到集合的末位
          */
         arrayListAdd();
         linkedListAdd();
+
+        /**
+         * 循环的时候，LinkedList比ArrayList 要慢；
+         */
+        arrayListIterator();
+        linkedListterator();
+    }
+
+    /**
+     * 如果Collection 为null，调用isEmpty()将会抛出异常
+     */
+    @Test
+    public void testIsEmpty(){
+        System.out.println(arrayList.isEmpty());
+        System.out.println(linkedList.isEmpty());
     }
 
     private void arrayListAdd() {
@@ -50,5 +67,38 @@ public class TestCollection {
         System.out.println("linkedList，结束时间是：" + end);
 
         System.out.println("linkedList消耗时间：" + (end - start));
+    }
+
+    private void arrayListIterator() {
+        long start = System.currentTimeMillis();
+        for (Person item : arrayList) {
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("arrayList循环消耗时间：" + (end - start));
+    }
+
+    private void linkedListterator() {
+        long start = System.currentTimeMillis();
+        linkedList.isEmpty();
+        for (Person item : linkedList) {
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("linkedList循环消耗时间：" + (end - start));
+    }
+
+    /**
+     * 集合是可以添加同一个对象的
+     */
+    @Test
+    public void testIterator(){
+        Person person1=new Person(1,"a");
+        arrayList.add(person1);
+        arrayList.add(person1);
+        arrayList.add(new Person(1,"b"));
+
+        Iterator<Person> iterator = arrayList.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
     }
 }
